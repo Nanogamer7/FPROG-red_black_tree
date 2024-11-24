f1370d442e5bd532e6f871b73361af485f3b72a2
Benedikt Galbavy <if22b082@technikum-wien.at>
2024-11-24 18:17:30 +0100

    red black tree (insert only)

--- main.hs
+++ main.hs
@@ -1,32 +1,42 @@
+import System.Random (randomRIO)
+
 data Color = Red | Black deriving Show -- `Show` makes it printable, according to the internet
 data Tree a = Empty | Node Color (Tree a) a (Tree a) deriving Show -- Tree is either empty, or consists of a Node with properties color, left subtree, value, and  right subtree 
 
 insert :: Ord a => a -> Tree a -> Tree a
-insert x t = ins t
+insert x t = makeBlack (ins t)
   where
-    ins Empty = Node Black Empty x Empty -- if inserting into a new tree, simply return a black node with two empty sub-trees, and the inserted value
+    ins Empty = Node Red Empty x Empty -- if inserting into a new tree, return a red node with two empty sub-trees, and the inserted value
     ins (Node color left y right) -- insert into existing tree
-      | x < y = Node color (ins left) y right -- call recursive on left side if new value is smaller
-      | x == y = Node color left y right -- do nothing; later increment word count I guess?
-      | x > y = Node color left y (ins right)
+      | x < y = balance (Node color (ins left) y right) -- call recursive on left side if new value is smaller
+      | x == y = Node color left y right -- do nothing; possible to increment word count if data type allows it
+      | x > y = balance (Node color left y (ins right))
+    makeBlack (Node _ left x right) = Node Black left x right -- root node needs to be black -> otherwise black violation if one of children empty
+
+insertList :: Ord a => [a] -> Tree a -> Tree a
+insertList [] tree = tree
+insertList (x:xs) tree = insertList xs (insert x tree) -- first element inserted, rest passed as list to next iteration
+
+balance :: Tree a -> Tree a -- order stays visually the same - trees and values stay in same order
+balance (Node _ (Node Red (Node Red tree1 x tree2) y tree3) z tree4) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
+balance (Node _ tree1 x (Node Red tree2 y (Node Red tree3 z tree4))) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
+balance (Node _ (Node Red tree1 x (Node Red tree2 y tree3)) z tree4) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
+balance (Node _ tree1 x (Node Red (Node Red tree2 y tree3) z tree4)) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
+balance (Node Red (Node Red tree1 x tree2) y (Node Red tree3 z tree4)) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
+balance tree = tree -- default case
 
 inOrder :: Tree a -> [a]
 inOrder Empty = []
 inOrder (Node _ left x right) = inOrder left ++ [x] ++ inOrder right -- recursively call with left subtree -> append value -> recursively call with right subtree
 
+-- for testing purposes
+generateRandomList :: Int -> IO [Int]
+generateRandomList n = mapM (const $ randomRIO (1, 100)) [1..n]
+
 main :: IO ()
 main = do
-  let emptyTree = Empty
-  let tree1 = insert 10 emptyTree -- insert first value
-  let tree2 = insert 5 tree1 -- insert smaller value
-  let tree3 = insert 15 tree2 -- insert larger value
-  let tree4 = insert 12 tree3 -- insert larger -> smaller value
-  let tree5 = insert 10 tree4 -- insert duplicate
+  numbers <- generateRandomList 15
+  let tree = insertList numbers Empty
 
-
-  print tree1
-  print tree2
-  print tree3
-  print tree4
-  print tree5
-  print (inOrder tree5)
+  print tree
+  print (inOrder tree)
