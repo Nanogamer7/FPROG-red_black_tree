28125dc7d47236fc47e7212948f6aff2f0386812
Benedikt Galbavy <if22b082@technikum-wien.at>
2024-11-25 15:24:16 +0100

    Improvements

--- main.hs
+++ main.hs
@@ -1,13 +1,15 @@
 import System.Random (randomRIO)
-import System.Random (randomRIO)
+import System.Environment (getArgs)
 import Data.Char (isAlpha, toLower)
 import Data.List.Split (splitOneOf)
+import Data.Char
 import qualified Data.Map as Map
 import System.IO (writeFile)
 
 data Color = Red | Black deriving Show -- `Show` makes it printable, according to the internet
 data Tree a = Empty | Node Color (Tree a) a (Tree a) deriving Show -- Tree is either empty, or consists of a Node with properties color, left subtree, value, and  right subtree 
 
+-- basic insert, not used
 insert :: Ord a => a -> Tree a -> Tree a
 insert x t = makeBlack (ins t)
   where
@@ -27,7 +29,7 @@ balance (Node _ (Node Red (Node Red tree1 x tree2) y tree3) z tree4) = Node Red 
 balance (Node _ tree1 x (Node Red tree2 y (Node Red tree3 z tree4))) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
 balance (Node _ (Node Red tree1 x (Node Red tree2 y tree3)) z tree4) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
 balance (Node _ tree1 x (Node Red (Node Red tree2 y tree3) z tree4)) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
-balance (Node Red (Node Red tree1 x tree2) y (Node Red tree3 z tree4)) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
+-- balance (Node Red (Node Red tree1 x tree2) y (Node Red tree3 z tree4)) = Node Red (Node Black tree1 x tree2) y (Node Black tree3 z tree4)
 balance tree = tree -- default case
 
 inOrder :: Tree a -> [a]
@@ -38,40 +40,35 @@ inOrder (Node _ left x right) = inOrder left ++ [x] ++ inOrder right -- recursiv
 generateRandomList :: Int -> IO [Int]
 generateRandomList n = mapM (const $ randomRIO (1, 100)) [1..n]
 
-insertWord :: Ord a => (a, Int) -> Tree (a, Int) -> Tree (a, Int)
-insertWord (word, count) Empty = Node Red Empty (word, count) Empty
-insertWord (word, count) (Node color left (w, c) right)
-  | word < w  = balance (Node color (insertWord (word, count) left) (w, c) right)
-  | word == w = Node color left (w, c + count) right -- Increment count if word matches
-  | word > w  = balance (Node color left (w, c) (insertWord (word, count) right))
-
-readWordsFromFile :: FilePath -> IO [String]
-readWordsFromFile path = do
-  content <- readFile path
-  let wordList = splitOneOf " ,.!?;:\n\r" content
-  return $ map (map toLower . filter isAlpha) $ filter (not . null) wordList
-
-{-
-countWords :: [String] -> Tree (String, Int)
-countWords words = foldr insertWord Empty (Map.toList wordCounts)
+insertWithCount :: Ord a => a -> Tree (a, Int) -> Tree (a, Int)
+insertWithCount x t = makeBlack (ins t)
   where
-    wordCounts = Map.fromListWith (+) [(word, 1) | word <- words]
-    insertWord (word, count) tree = insert (word, count) tree
--}
+    ins Empty = Node Red Empty (x, 1) Empty
+    ins (Node color left (y, count) right)
+      | x < y  = balance (Node color (ins left) (y, count) right)
+      | x == y = Node color left (y, count + 1) right -- Increment count if x matches
+      | x > y  = balance (Node color left (y, count) (ins right))
+    makeBlack (Node _ left x right) = Node Black left x right
 
-countWords :: [String] -> Tree (String, Int)
-countWords = foldr (\word tree -> insertWord (word, 1) tree) Empty
+tokenize :: String -> [String]
+tokenize str = words (map (\char -> case () of
+        _ | isAlpha char -> toLower char
+          | isDigit char -> char
+          | otherwise    -> ' ') (filter (not . isPunctuation) str))
+
+insertListWithCount :: [String] -> Tree (String, Int)
+insertListWithCount = foldr (\word tree -> insertWithCount word tree) Empty
 
 main :: IO ()
 main = do
-  putStrLn "Enter the path of the text file to process:"
-  filePath <- getLine
-  wordsList <- readWordsFromFile filePath
-  let wordTree = countWords wordsList
-
-  --putStrLn "In-order traversal of the tree with word counts:"
-  --print (inOrder wordTree)
-  let inOrderOutput = inOrder wordTree
-  let outputFileName = "countedWords.txt"
-  writeFile outputFileName (unlines (map show inOrderOutput))
-  putStrLn $ "Word counts saved to: " ++ outputFileName
+  args <- getArgs
+  case args of
+    [filePath] -> do
+      fullText <- readFile filePath
+      let wordsList = tokenize fullText
+      let wordTree = insertListWithCount wordsList
+      let inOrderOutput = inOrder wordTree
+      let outputFileName = "output.txt"
+      writeFile outputFileName (unlines (map show inOrderOutput))
+      putStrLn $ "Word counts saved to: " ++ outputFileName
+    _ -> putStrLn "Usage: <program> <input-file-path>"
